01a3a3ec4aff75871ddc15276fa6ccc6c61b6033
n.antonyan <n.antonyan@bftcom.com>
2024-01-05 23:21:21 +0300

    Создал класс и методы для парсинга bybit

--- bybit.py
+++ bybit.py
@@ -1,56 +1,42 @@
 import requests
 import json
 
-data = {
-    "amount": "",
-    "authMaker": False,
-    "canTrade": False,
-    "currencyId": "RUB",
-    "page": "1",
-    "payment": [],
-    "side": "1",
-    "size": "10",
-    "tokenId": "USDT",
-    "userId": ""
-}
-result = requests.post("https://api2.bybit.com/fiat/otc/item/online", json=data)
-result = json.loads(result.text)
-print(result)
-print("SELL:")
-for data in result['result']['items']:
-    price = data['price']
-    currency = data['currencyId']
-    minAmount = data['minAmount']
-    maxAmount = data['maxAmount']
-    initAmount = data['lastQuantity']
-    tradeMethod = data['payments'][0] # только первый метод оплаты, надо фиксить, чтобы были все
-    makerOrdersNum = data['recentOrderNum']
-    makerOrdersRate = data['recentExecuteRate']
-    print(price, currency, minAmount, 'RUB -', maxAmount, 'RUB', initAmount, 'USDT', tradeMethod, makerOrdersNum, 'сделок', makerOrdersRate, 'рейтинг успешных', sep=" ")
 
-data = {
-    "amount": "",
-    "authMaker": False,
-    "canTrade": False,
-    "currencyId": "RUB",
-    "page": "1",
-    "payment": [],
-    "side": "0",
-    "size": "10",
-    "tokenId": "USDT",
-    "userId": ""
-}
-result = requests.post("https://api2.bybit.com/fiat/otc/item/online", json=data)
-result = json.loads(result.text)
-print(result)
-print("BUY:")
-for data in result['result']['items']:
-    price = data['price']
-    currency = data['currencyId']
-    minAmount = data['minAmount']
-    maxAmount = data['maxAmount']
-    initAmount = data['lastQuantity']
-    tradeMethod = data['payments'][0] # только первый метод оплаты, надо фиксить, чтобы были все
-    makerOrdersNum = data['recentOrderNum']
-    makerOrdersRate = data['recentExecuteRate']
-    print(price, currency, minAmount, 'RUB -', maxAmount, 'RUB', initAmount, 'USDT', tradeMethod, makerOrdersNum, 'сделок', makerOrdersRate, 'рейтинг успешных', sep=" ")
+class BybitParse():
+
+    def getPrices(self, type="buy", size="20", token="USDT", currency="RUB"):
+        dataRequest = {
+            "amount": "",
+            "authMaker": False,
+            "canTrade": False,
+            "currencyId": currency,
+            "page": "1",
+            "payment": [],
+            "side": "1",
+            "size": size,
+            "tokenId": token,
+            "userId": ""
+        }
+        if type == "sell":
+            dataRequest['side'] = "0"
+
+        result = requests.post("https://api2.bybit.com/fiat/otc/item/online", json=dataRequest)
+        result = json.loads(result.text)
+        currSellData = {}
+        resultData = []
+        for data in result['result']['items']:
+            currSellData['price'] = data['price']
+            currSellData['currency'] = data['currencyId']
+            currSellData['minAmount'] = data['minAmount']
+            currSellData['maxAmount'] = data['maxAmount']
+            currSellData['initAmount'] = data['lastQuantity']
+            currSellData['payments'] = data['payments'][0] # только первый метод оплаты, надо фиксить, чтобы были все
+            currSellData['makerOrdersNum'] = data['recentOrderNum']
+            currSellData['makerOrdersRate'] = data['recentExecuteRate']
+            resultData.append(currSellData.copy())
+        return resultData
+
+    def printData(self, resultData):
+        for data in resultData:
+            print(data['price'], data['currency'], data['minAmount'], 'RUB -', data['maxAmount'], 'RUB', data['initAmount'], 'USDT', data['payments'],
+                  data['makerOrdersNum'], 'сделок', data['makerOrdersRate'], 'рейтинг успешных', sep=" ")
